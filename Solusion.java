217811ce8f0c86da425fbdb78e5d05f3825dfc64
guodefeng <guoguo535201@126.com>
2020-09-13 08:18:09 +0800

    测试 提交

---
+++ Solusion.java
@@ -0,0 +1,31 @@
+package com.peter.demo;
+
+/**
+ * @author ：defeng.guo
+ * @date ：Created in 2020/8/31 10:57
+ * @description：给定两个字符串s和t， 写一个函数来决定是否t是s的重组词。你可以假设字符串只包含小写字母
+ * @modified By：
+ * @version: $
+ */
+public class Solusion {
+
+  public boolean isAnagram(String s, String t) {
+    if(s.length()!=t.length())
+      return false;
+    int bit[] = new int[26];
+    for(int i=0;i<s.length();i++){
+      bit[s.charAt(i)-'a']++;
+    }
+
+    for(int i=0;i<s.length();i++){
+      if(--bit[t.charAt(i)-'a']<0)
+        return false;
+    }
+    return true;
+  }
+  public static void main(String[] args){
+     Solusion s = new Solusion();
+     boolean test = s.isAnagram("abc","bcz");
+    System.out.println(test);
+  }
+}
